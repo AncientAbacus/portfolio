71c50205b971d9cb5bc316e847c91844431651da
Gino Angelici <59946802+AncientAbacus@users.noreply.github.com>
2025-01-25 00:04:25 -0800

    Update global.js

--- global.js
+++ global.js
@@ -19,18 +19,19 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 for (let p of pages) {
     let url = p.url;
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
     let title = p.title;
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    a.classList.toggle(
-        'current',
-        a.host === location.host && a.pathname === location.pathname
-    );
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
     if (a.host !== location.host) {
         a.target = "_blank";
-    }
+      }
     nav.append(a);
 }
 
