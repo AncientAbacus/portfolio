4231c3b2cf0fe20427a8bca708e8dcc460197162
Gino Angelici <59946802+AncientAbacus@users.noreply.github.com>
2025-02-05 14:27:00 -0800

    break

--- global.js
+++ global.js
@@ -95,7 +95,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
       const article = document.createElement('article');
       article.innerHTML = `
           <${headingLevel}>${project.title}</${headingLevel}>
-          <img src="${basePath}/${project.image}" class="${project.imageClass}" alt="${project.title}">
+          <img src="${project.image}" class="${project.imageClass}" alt="${project.title}">
           <p>${project.description}</p>
       `;
       containerElement.appendChild(article);
